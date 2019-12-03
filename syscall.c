5ae67ad52db8eb3d21b7ac4fa4771bb956c5e70b
amir2040 <1377amirhossein1998@gmail.com>
2019-12-03 19:54:53 +0330

    syscall function modified for incrementing corresponding element of syscall_counter

--- syscall.c
+++ syscall.c
@@ -60,7 +60,7 @@ argptr(int n, char **pp, int size)
 {
   int i;
   struct proc *curproc = myproc();
- 
+
   if(argint(n, &i) < 0)
     return -1;
   if(size < 0 || (uint)i >= curproc->sz || (uint)i+size > curproc->sz)
@@ -137,6 +137,7 @@ syscall(void)
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     curproc->tf->eax = syscalls[num]();
+    curproc->syscall_counter[num]++;
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
