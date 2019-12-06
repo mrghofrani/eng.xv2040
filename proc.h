55d78e9e67374dbb4543eb81dd46a51dfde0faa8
amir2040 <1377amirhossein1998@gmail.com>
2019-12-06 17:17:33 +0330

    added priority and calculatedPriority to proc struct

--- proc.h
+++ proc.h
@@ -49,6 +49,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int priority;                // priority of the process
+  int calculatedPriority       // Calculated priority for each process. This part is updated during running a process
 };
 
 // Process memory is laid out contiguously, low addresses first:
