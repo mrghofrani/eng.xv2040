f6bb1c90ff5406ce82b9970655d6b3082da0279d
amir2040 <1377amirhossein1998@gmail.com>
2019-12-16 13:03:23 +0330

    A bug related to managing lock in scheduler has been resolved.

--- proc.c
+++ proc.c
@@ -372,12 +372,12 @@ scheduler(void)
                 break;
         }
     }
+    acquire(&ptable.lock);
     if(algorithm == 2) {
         min_calculatedPriority = INT_MAX;
         found = 0; // Zero means not found
 
         // Loop over process table looking for process to run.
-        acquire(&ptable.lock);
         for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
             if (p->state == RUNNABLE && min_calculatedPriority > p->calculatedPriority) {
                 min_calculatedPriority = p->calculatedPriority;
@@ -401,7 +401,6 @@ scheduler(void)
             // It should have changed its p->state before coming back.
             c->proc = 0;
         }
-        release(&ptable.lock);
     }
     else {
         for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
@@ -422,6 +421,7 @@ scheduler(void)
             c->proc = 0;
         }
     }
+      release(&ptable.lock);
   }
 }
 
