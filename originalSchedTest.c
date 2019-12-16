bce213f23a6bbf1b80521632a444e4d5a3c378c1
Mohammadreza Ghofrani <mohammadrezaghofrany@gmail.com>
2019-12-16 21:45:27 +0330

    originalSchedTest implemented

---
+++ originalSchedTest.c
@@ -0,0 +1,45 @@
+//
+// Created by amir2040 on 12/16/19.
+//
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#define PROCESS_NUMBER 10
+#define ITERATION 1000
+
+int main(){
+    int pid = getpid();
+    for(int i = 0 ; i < 10 ; ++i) { // TODO: Think 15 minutes on this to figure out B-)
+        if (pid > 0)
+            pid = fork();
+    }
+    if(pid > 0){
+        struct timeVariable tv;
+        struct timeVariable prc[PROCESS_NUMBER];
+        int chid[PROCESS_NUMBER];
+        int wt = 0;
+        int cbt = 0;
+        int chpid;
+        for (int i = 0; i < PROCESS_NUMBER; ++i) {
+            chpid = waitForChild(&tv);
+            chid[i] = chpid;
+            prc[i] = tv;
+            wt += tv.readyTime;
+            cbt += tv.runningTime;
+        }
+        for (int i = 0; i < PROCESS_NUMBER; ++i) {
+            tv = prc[i];
+            chpid = chid[i];
+            printf(1, "Process %d left created %d, run duration was %d, slept for %d, was ready for %d, terminated at %d\n",
+                   chpid, tv.creationTime, tv.runningTime, tv.sleepingTime, tv.readyTime, tv.terminationTime);
+            printf(1, "CBT: %d, WT: %d\n", tv.runningTime, tv.readyTime);
+        }
+        printf(1, "Average WT: %d, Average CBT: %d\n", wt/PROCESS_NUMBER, cbt/PROCESS_NUMBER);
+        exit();
+    }
+    else{
+        for (int i = 0; i < ITERATION; ++i)
+            printf(1, "%d: %d\n", getpid(), i);
+        exit();
+    }
+}
