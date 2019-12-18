b6fc5754e1f8a6bdcfae1ad846245e8aa6e67b55
Mohammadreza Ghofrani <mohammadrezaghofrany@gmail.com>
2019-12-18 23:05:58 +0330

    Added report file

--- prioritySchedTest.c
+++ prioritySchedTest.c
@@ -32,27 +32,32 @@ int main(){
         }
         int lwt = 0; // local waiting time
         int lcbt = 0; // local Cognitive behavioral therapy
+        int ltt = 0;
         int gwt = 0; // global waiting time
         int gcbt = 0; // global Cognitive behavioral therapy
+        int gtt = 0;
         for (int i = 0; i < PRIORITY_LEVEL; ++i) {
             lwt = 0;
             lcbt = 0;
+            ltt = 0;
             for (int j = 0; j < PROCESS_NUMBER / PRIORITY_LEVEL; ++j) {
                 tv = prc[i][j];
                 chpid = chid[i][j];
                 printf(1, "Process %d left created %d, run duration was %d, slept for %d, was ready for %d, terminated at %d\n",
                        chpid, tv.creationTime, tv.runningTime, tv.sleepingTime, tv.readyTime, tv.terminationTime);
-                printf(1, "CBT: %d, WT: %d, TT:\n", tv.runningTime, tv.readyTime);
+                printf(1, "CBT: %d, WT: %d, TT: %d\n", tv.runningTime, tv.readyTime, (tv.terminationTime - tv.creationTime));
                 lwt += tv.readyTime;
                 lcbt += tv.runningTime;
+                ltt += (tv.terminationTime - tv.creationTime);
             }
             printf(1, "---------->");
-            printf(1, "Group %d: Average WT %d, Average CBT %d\n", PRIORITY_LEVEL-i, lwt/(PROCESS_NUMBER / PRIORITY_LEVEL), lcbt/(PROCESS_NUMBER / PRIORITY_LEVEL));
+            printf(1, "Group %d: Average WT %d, Average CBT %d, Average TT %d\n", PRIORITY_LEVEL-i, lwt/(PROCESS_NUMBER / PRIORITY_LEVEL), lcbt/(PROCESS_NUMBER / PRIORITY_LEVEL), ltt/(PROCESS_NUMBER / PRIORITY_LEVEL));
             gcbt += lcbt;
             gwt += lwt;
+            gtt += ltt;
         }
-        printf(1, "Total Average WT: %d, Average CBT: %d\n", gwt/PRIORITY_LEVEL, gcbt/PRIORITY_LEVEL);
-        exit();
+        printf(1, "Total Average WT %d, Average CBT %d, Average TT %d\n", gwt/PROCESS_NUMBER, gcbt/PROCESS_NUMBER, gtt/PROCESS_NUMBER);
+         exit();
     }
     else{
         for (int i = 0; i < ITERATION; ++i)
